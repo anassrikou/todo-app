a5bdad368d9e5fe2f4c3f41848478bae69bb5e21
revaxl <robin208@hotmail.fr>
2019-05-05 18:36:06 +0100

    create and read todos from json file

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
         success: function(response, status) {
             console.log(response, status);
             // do nothing is list is empty
-            if (response.length === 0) return;
+            if (response.length === 0 || response.error) return;
 
             response.forEach(element => {
-               renderTodo(element.todo)
+               renderTodo(element)
             });
 
         },
@@ -22,11 +22,12 @@ $(document).ready(function() {
 
 });
 
-function renderTodo(todo) {
+function renderTodo(item) {
+    console.log("rendering todo", item);
     const todo_list = document.querySelector('.todo--list-group');
     const markup = `
-    <div class="list-group-item">
-        ${todo}
+    <div class="list-group-item" data-id=${item.id}>
+        ${item.todo}
         <div class="btn-group float-right" role="group" aria-label="Basic example">
         <button class="btn btn-light"><i class="fas fa-check"></i></button>
         <button class="btn btn-light"><i class="far fa-trash-alt"></i></button>
@@ -50,12 +51,12 @@ function handleSubmit(e) {
         method: 'POST',
         data: data,
         success: function(response, status, xhr) {
-            console.log(data, status, xhr);
+            console.log(response, status, xhr);
             // clear the input field
             document.querySelector('#todo-form').reset();
 
             // get the data from form
-            const todo_item = data[0].value;
+            const todo_item = { todo: data[0].value, done: false };
 
             renderTodo(todo_item);
         },
--- routes/todos.js
+++ routes/todos.js
@@ -1,18 +1,38 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const uniqid = require('uniqid');
 
 const router = express.Router();
-const todolist = [];
+
+// get the todo file
+const todo_file = path.join(__dirname, '../todos.json');
 
 /* GET users listing. */
 router.get('/todos', (req, res, next) => {
-  res.json(todolist);
+  fs.readFile(todo_file, 'UTF-8', (error, data) => {
+    if (error) return res.json( { error });
+    console.log(data);
+    res.json(JSON.parse(data));
+  })
 });
 
 router.post('/todos', (req, res) => {
   console.log(req.body);
-  todolist.push(req.body);  
-  console.log(todolist);
-  res.json({ message : 'ok' });
+  const id = uniqid.time();
+  const new_todo = { ...req.body, id };
+  fs.readFile(todo_file, async (error, data) => {
+    if (error) await createNewTodoFile();
+    // get all todos 
+    const all_todos = JSON.parse(data);
+    // then append the new one
+    all_todos.push(new_todo); 
+    // and write to file
+    fs.writeFile(todo_file, JSON.stringify(all_todos), 'UTF-8', (error) => {
+      if (error) return res.json({ error });
+      res.json({ message : 'ok' });
+    })
+  }) ;
 });
 
 router.put('/todos/:id', (req, res) => {
@@ -21,6 +41,10 @@ router.put('/todos/:id', (req, res) => {
 
 router.delete('/todos/:id', (req, res) => {
   res.json({ 'deleted': 'ok' });
-})
+});
+
+function createNewTodoFile() {
+  fs.writeFile(todo_file, "[]", error => error);
+}
 
 module.exports = router;
