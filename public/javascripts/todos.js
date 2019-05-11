349a644662050b36544075041a0ca1ed1ba06d70
revaxl <robin208@hotmail.fr>
2019-05-11 14:53:45 +0100

    added buttons for done and delete todos

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,36 +1,41 @@
-$(document).ready(function() {
-    const todo_form = document.querySelector('#todo-form');
-    todo_form.addEventListener('submit', handleSubmit);
+$(document).ready(function () {
+    const todo_form = $('#todo-form');
+    const todo_list = $('.todos__list');
+    
+    todo_form.on('submit', handleSubmit);
+    todo_list.on('click', handleBtnClick);
 
     // get all todos from server
-    $.ajax({
-        url: 'api/todos',
-        success: function(response, status) {
-            console.log(response, status);
-            // do nothing is list is empty
-            if (response.length === 0 || response.error) return;
-
-            response.forEach(element => {
-               renderTodo(element)
-            });
-
-        },
-        error: function(xhr, status, message) {
-            console.log(message);
-        }
-    });
+    getAllTodos();
 
 });
+
+function getAllTodos() {
+    $.ajax({
+        url: 'api/todos',
+        success: function (response, status) {
+            console.log(response, status);
+            // do nothing is list is empty or there's an error
+            if (response.length === 0 || response.error) return;
+
+            response.forEach(element => renderTodo(element));
+
+        },
+        error: function (xhr, status, message) {
+            console.log(status, message);
+        }
+    });
+}
 
 function renderTodo(item) {
     console.log("rendering todo", item);
     const todo_list = document.querySelector('.todo--list-group');
     const markup = `
     <div class="list-group-item" data-id=${item.id}>
-        ${item.todo}
+        <span class="todo__text">${item.todo}</span>
         <div class="btn-group float-right" role="group" aria-label="Basic example">
-        <button class="btn btn-light"><i class="fas fa-check"></i></button>
-        <button class="btn btn-light"><i class="far fa-trash-alt"></i></button>
+        <button class="btn btn-success todo-done">done</button>
+        <button class="btn btn-danger todo-delete">delete</button>
         </div>
     </div>
     `;
@@ -40,27 +45,74 @@ function renderTodo(item) {
 function handleSubmit(e) {
     // prevent form from submitting and refreshing the page
     e.preventDefault();
-    
+
     // get the form data
     const data = $('#todo-form').serializeArray();
     console.log(data);
+    data.push({ name: 'done', value: false });
 
     // send ajax request to the todos api
     $.ajax({
         url: 'api/todos',
         method: 'POST',
         data: data,
-        success: function(response, status, xhr) {
+        success: function (response, status, xhr) {
             console.log(response, status, xhr);
             // clear the input field
             document.querySelector('#todo-form').reset();
 
-            // get the data from form
-            const todo_item = { todo: data[0].value, done: false };
-
-            renderTodo(todo_item);
+            renderTodo(response.new_todo);
         },
-        error: function(xhr, status, message) {
+        error: function (xhr, status, message) {
+            console.log(xhr, status, message);
+        },
+    });
+}
+
+
+function handleBtnClick(e) {
+    console.log(e.target);
+    if (e.target.tagName !== "BUTTON") return;
+
+    // get the whole selected element
+    const todo = $(e.target).parentsUntil($('.todo--list-group'));
+
+    // get the actual button not whats inside it
+    const clickedbtn = e.target ;
+    console.log("clicked btn", clickedbtn);
+    if (clickedbtn.classList.toString().includes('done')) return todoDone(todo);
+    else if (clickedbtn.classList.toString().includes('delete')) return todoDelete(todo);
+    else alert('error');
+}
+
+function todoDone(todo) {
+    console.log("done" , todo);
+    $.ajax({
+        url: `api/todos/${todo[1].dataset.id}`,
+        method: 'PUT',
+        success: function (response, status, xhr) {
+            console.log(response, status, xhr);
+            // update the element classes
+            console.log("done todo", todo);
+            todo[1].classList.add('completed');
+        },
+        error: function (xhr, status, message) {
+            console.log(xhr, status, message);
+        },
+    });
+}
+
+function todoDelete(todo) {
+    console.log("delete", todo);
+    $.ajax({
+        url: `api/todos/${todo[1].dataset.id}`,
+        method: 'DELETE',
+        success: function (response, status, xhr) {
+            console.log(response, status, xhr);
+            // remove the element from the dom
+            todo.remove();
+        },
+        error: function (xhr, status, message) {
             console.log(xhr, status, message);
         },
     });
--- routes/todos.js
+++ routes/todos.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // get the todo file
 const todo_file = path.join(__dirname, '../todos.json');
 
-/* GET users listing. */
+/* GET todo list. */
 router.get('/todos', (req, res, next) => {
   fs.readFile(todo_file, 'UTF-8', (error, data) => {
     if (error) return res.json( { error });
@@ -18,29 +18,82 @@ router.get('/todos', (req, res, next) => {
 });
 
 router.post('/todos', (req, res) => {
-  console.log(req.body);
+  console.log('req bodo', req.body);
   const id = uniqid.time();
-  const new_todo = { ...req.body, id };
+  const new_todo = { todo: req.body.todo , done: false, id };
   fs.readFile(todo_file, async (error, data) => {
-    if (error) await createNewTodoFile();
+    try {
+      if (error) await createNewTodoFile();
+      // get all todos 
+      const all_todos = JSON.parse(data);
+      // then append the new one
+      all_todos.push(new_todo); 
+      // and write to file
+      fs.writeFile(todo_file, JSON.stringify(all_todos), 'UTF-8', (error) => {
+        if (error) return res.json({ error });
+        res.json({ message : 'ok', new_todo });
+      });
+    } catch (error) {
+      res.json ({ error });
+    }
+  });
+});
+
+router.put('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  console.log('params', id);
+
+  fs.readFile(todo_file, (error, data) => {
+    if (error) return res.json({ err });
     // get all todos 
     const all_todos = JSON.parse(data);
-    // then append the new one
-    all_todos.push(new_todo); 
+    // then try to find the old todo for updating
+    old_todo = all_todos.findIndex(todo => todo.id === id);
+
+    // if old todo doesnt exist then create new one with the give id
+    if (old_todo === -1) {
+      all_todos.push({ todo: req.body.todo , done: false, id: id });
+      console.log('didnt find it');
+    }
+    // if todo is found, then we update it
+    else {
+      all_todos[old_todo].done = !all_todos[old_todo].done;
+      console.log('found it');
+    }
     // and write to file
     fs.writeFile(todo_file, JSON.stringify(all_todos), 'UTF-8', (error) => {
       if (error) return res.json({ error });
       res.json({ message : 'ok' });
     })
-  }) ;
-});
-
-router.put('/todos/:id', (req, res) => {
-  res.json({ 'update': 'ok' });
+  });
 });
 
 router.delete('/todos/:id', (req, res) => {
-  res.json({ 'deleted': 'ok' });
+  const id = req.params.id;
+  console.log('params', id);
+  fs.readFile(todo_file, (error, data) => {
+    if (error) return res.json({ err });
+    // get all todos 
+    const all_todos = JSON.parse(data);
+    // then try to find the old todo for updating
+    old_todo = all_todos.findIndex(todo => todo.id === id);
+
+    // if old todo doesnt exist then create new one with the give id
+    if (old_todo === -1) {
+      console.log('didnt find it');
+      return res.json( { message: 'didnt find it'});
+    }
+    // if todo is found, then we update it
+    else {
+      all_todos.splice(old_todo, 1);
+      console.log('found it');
+    }
+    // and write to file
+    fs.writeFile(todo_file, JSON.stringify(all_todos), 'UTF-8', (error) => {
+      if (error) return res.json({ error });
+      res.json({ message : 'ok' });
+    })
+  });
 });
 
 function createNewTodoFile() {
